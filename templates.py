5892408ddf94869d8af1ae07c13716a9748cefe1
liuge1994 <610562301@qq.com>
2015-11-16 21:45:50 +0800

    templates.

---
+++ templates.py
@@ -0,0 +1,32 @@
+# templates.py 
+
+import fileinput, re
+
+# match 
+pattern = r'\[(.+?)\]'
+field_pat = re.compile(pattern)
+
+# 
+scope = {}
+
+#
+def replacement(match):
+	code = match.group(1)
+	try:
+		#
+		return str(eval(code, scope))
+	except SyntaxError:
+		#
+		exec(code, scope)
+		#
+		return ''
+
+#
+#
+lines = []
+for line in fileinput.input():
+	lines.append(line)
+text = ''.join(lines)
+
+#
+print(field_pat.sub(replacement, text))
